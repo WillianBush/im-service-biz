26eaa4510c1baedc3a18c3d71aaa8205a0ab7d6f
robot <robot@superVideo.com>
2022-09-20 17:30:17 +0800

    表格和导出调整

--- src/main/java/net/chenlin/dp/common/utils/GenerateRandomCode.java
+++ src/main/java/net/chenlin/dp/common/utils/GenerateRandomCode.java
@@ -1,10 +1,16 @@
 package net.chenlin.dp.common.utils;
 
 import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import net.chenlin.dp.modules.schedule.entity.QiLingResponse;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class GenerateRandomCode {
@@ -13,8 +19,7 @@ public class GenerateRandomCode {
     private final static String CHECK_DOMAIN_QQ = "http://api.uouin.com/app/qq?username=sadiiv983&key=csl6kHZSDVztIUW&url=";
 
     private static final String SHORT_STR = "0123456789";
-    private static final String[] SHORT_DOMAIN_STUFF = {"tv","pw","co","cc","nl","in","io","bz","im","it","la","nz","ph"
-    ,"pw","ws"};
+    private static final String[] SHORT_DOMAIN_STUFF = {"tv","pw","co","cc","nl","in","io","bz","im","it","nz","ph","pw","ws"};
 
     private static final String qqRegisterDomain = "syxicecream.com";
 
@@ -99,34 +104,18 @@ public class GenerateRandomCode {
 
     public static void generateRandomAndChechedShortDoamins(){
         int count = 300;
-        Set<String> listStringNormal = new HashSet<>();
+        Set<String> shortLinkBeforeCheck = new HashSet<>();
         // 短域名后缀 15 个
         System.out.println("生成 300 个短域名");
         for (int i = 0; i <= count / SHORT_DOMAIN_STUFF.length; i++) {
             String str = getRandomCodeMath(4,1).get(0);
             for (String domainStuff : SHORT_DOMAIN_STUFF) {
                 String domain = str + "." + domainStuff;
-                try {
-                    String checkDomainQQ = CHECK_DOMAIN_QQ + domain;
-                    String resp = OKhttpUtil.httpGet(checkDomainQQ, null);
-                    QiLingResponse response = JSONObject.parseObject(resp, QiLingResponse.class);
-                    if (response != null && !response.getStatu()) {
-                        log.info("域名：{} 已经被封",domain);
-                        if (response.getCount() != null || response.getDescribe() != null || response.getReason() != null) {
-                            log.error("麒麟接口返回异常， resp:{}",response);
-                        }
-                    }else {
-                        listStringNormal.add(domain);
-                    }
-
-//                    listStringNormal.add(domain);
-                    Thread.sleep(1600);
-                } catch (Exception e) {
-                    log.error("",e);
-                }
+                shortLinkBeforeCheck.add(domain);
             }
         }
-        for (String str :  listStringNormal) {
+        List<String> shortLinkListChecked = check(new ArrayList<>(shortLinkBeforeCheck));
+        for (String str :  shortLinkListChecked) {
             System.out.println(str);
         }
     }
@@ -161,26 +150,7 @@ public class GenerateRandomCode {
 
     public static void  generateRandomAndChechedqqRegisterDomain(){
         List<String> listString = getRandomCodeDomain(6,200,"xyz");
-        List<String> listStringNormal = new ArrayList<>();
-        for(String domain : listString){
-            try {
-                String checkDomainQQ = CHECK_DOMAIN_QQ + domain;
-                String resp = OKhttpUtil.httpGet(checkDomainQQ, null);
-                QiLingResponse response = JSONObject.parseObject(resp, QiLingResponse.class);
-                if (response != null && !response.getStatu()) {
-                    log.info("域名：{} 已经被封",domain);
-                    if (response.getCount() != null || response.getDescribe() != null || response.getReason() != null) {
-                        log.error("麒麟接口返回异常， resp:{}",response);
-                    }
-                }else {
-                    listStringNormal.add(domain);
-                }
-//                listStringNormal.add(domain);
-                Thread.sleep(1600);
-            } catch (Exception e) {
-                log.error("",e);
-            }
-        }
+        List<String> listStringNormal = check(listString);
         for (String str :  listStringNormal) {
             System.out.println(str);
         }
@@ -190,9 +160,78 @@ public class GenerateRandomCode {
         // 生成随机落地页
 //        generateRandomAndChechedqqRegisterDomain();
         // 生成随机短域名
-//        generateRandomAndChechedShortDoamins();
+        generateRandomAndChechedShortDoamins();
         // 生成 qq绿标
-        generateRandomAndChechedQQDoamins();
+//        generateRandomAndChechedQQDoamins();
     }
 
+
+
+    public static List<String>  check(List<String> domainEnables) {
+
+        int group = domainEnables.size()/ 25;
+        int left = domainEnables.size() % 25;
+        if (left != 0){
+            group ++;
+        }
+        long start = System.currentTimeMillis();
+        List<List<String>> domainEnableGroup = Lists.partition(domainEnables,group);
+
+        // 微信检测不准，去掉
+//                String checkDomainWechat = CHECK_DOMAIN_WECHAT + domain.getDomainName();
+//                doCheck(domain, checkDomainWechat);
+        CopyOnWriteArrayList<String> checked = new CopyOnWriteArrayList<>();
+        for (List<String> domainSub : domainEnableGroup) {
+            for (String domain : domainSub) {
+                String checkDomainQQ = CHECK_DOMAIN_QQ + domain;
+                CompletableFuture<String> f =  CompletableFuture.supplyAsync(()->doCheck( checkDomainQQ,domain));
+                f.whenComplete(new BiConsumer<String, Throwable>() {
+                    @Override
+                    public void accept(String amap, Throwable throwable) {
+                        checked.add(amap);
+                    }
+                });
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    log.error("",e);
+                }
+            }
+            try {
+                Thread.sleep(1100);
+            } catch (InterruptedException e) {
+                log.error("",e);
+            }
+        }
+        while (checked.size() != domainEnables.size()){
+            log.info("正在执行,请等待结果 ...");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                log.error("",e);
+            }
+        }
+        long spendSeconds = (System.currentTimeMillis() -start) / 1000 ;
+        List<String> domainEnablesChecked = checked.stream().filter(it->it != null).collect(Collectors.toList());
+        log.info("[check] 检测域名完毕....  检测前:{}；检测后：{}",domainEnables.size(),domainEnablesChecked.size());
+        return domainEnablesChecked;
+    }
+
+    private static String doCheck( String checkDomain,String domain) {
+        try {
+            String resp = OKhttpUtil.httpGet(checkDomain, null);
+            QiLingResponse response = JSONObject.parseObject(resp, QiLingResponse.class);
+            if (response != null && !response.getStatu()) {
+                log.info("域名：{} 已经被封",checkDomain);
+                if (response.getCount() != null || response.getDescribe() != null || response.getReason() != null) {
+                    log.error("麒麟接口返回异常， resp:{}",response);
+                }
+            }else {
+                return domain;
+            }
+        } catch (IOException e) {
+            log.error("异常,checkDomain:{}",domain,e);
+        }
+        return null;
+    }
 }
