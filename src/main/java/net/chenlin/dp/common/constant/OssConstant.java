0a15c4bf8c64dbc28251f26247b3ac2458de9fb0
Lupl@lupenglong <lupenglong@hlinkage.com>
2022-10-23 17:00:09 +0800

    界面配置上传到OSS

---
+++ src/main/java/net/chenlin/dp/common/constant/OssConstant.java
@@ -0,0 +1,7 @@
+package net.chenlin.dp.common.constant;
+
+public class OssConstant {
+
+    public final static String OSS_CONFIG_PATH = "config_sys";
+    public final static String OSS_SHOW_FILE_NAME = "show_config.txt";
+}
--- src/main/java/net/chenlin/dp/modules/biz/show/service/impl/ShowConfigServiceImpl.java
+++ src/main/java/net/chenlin/dp/modules/biz/show/service/impl/ShowConfigServiceImpl.java
@@ -1,9 +1,17 @@
 package net.chenlin.dp.modules.biz.show.service.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.List;
 
+import com.alibaba.fastjson2.JSONObject;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.chenlin.dp.common.constant.OssConstant;
+import net.chenlin.dp.common.entity.OSSUploadResp;
+import net.chenlin.dp.common.utils.OssUtil;
 import org.springframework.stereotype.Service;
 
 import net.chenlin.dp.common.entity.Page;
@@ -19,11 +27,13 @@ import net.chenlin.dp.modules.biz.show.service.ShowConfigService;
  * 
  * @author wang<fangyuan.co@outlook.com>
  */
+@Slf4j
 @Service("showConfigService")
 @AllArgsConstructor
 public class ShowConfigServiceImpl implements ShowConfigService {
 
     private ShowConfigMapper showConfigMapper;
+	private OssUtil ossUtil;
 
     /**
      * 分页查询
@@ -47,6 +57,9 @@ public class ShowConfigServiceImpl implements ShowConfigService {
 	@Override
 	public Resp<ShowConfigEntity> saveShowConfig(ShowConfigEntity showConfig) {
 		int count = showConfigMapper.save(showConfig);
+		if(count>0){
+			upLoadConfig(showConfig);
+		}
 		return CommonUtils.msgResp(count);
 	}
 
@@ -69,6 +82,9 @@ public class ShowConfigServiceImpl implements ShowConfigService {
 	@Override
 	public Resp<Integer> updateShowConfig(ShowConfigEntity showConfig) {
 		int count = showConfigMapper.update(showConfig);
+		if(count>0){
+			upLoadConfig(showConfig);
+		}
 		return CommonUtils.msgResp(count);
 	}
 
@@ -81,6 +97,21 @@ public class ShowConfigServiceImpl implements ShowConfigService {
 	public Resp batchRemove(String[] id) {
 		int count = showConfigMapper.batchRemove(id);
 		return CommonUtils.msgResp(id, count);
+	}
+
+	/**
+	 * 上传配置
+	 * @param showConfigEntity
+	 */
+	public void upLoadConfig(ShowConfigEntity showConfigEntity){
+		try {
+			byte[] encryptBytes =  showConfigEntity.toString().getBytes(StandardCharsets.UTF_8);
+			InputStream in = new ByteArrayInputStream(encryptBytes);
+			OSSUploadResp resp =  ossUtil.uploadObjectToOSS(in, OssConstant.OSS_SHOW_FILE_NAME,OssConstant.OSS_CONFIG_PATH, (long) in.available());
+			log.debug("oss上传结束:{}", JSONObject.toJSONString(resp));
+		} catch (Exception e) {
+			log.error("ShowConfigServiceImpl:{}",e);
+		}
 	}
 
 }
