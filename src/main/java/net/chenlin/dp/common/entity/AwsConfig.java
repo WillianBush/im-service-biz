c571e9d89a52b58f30c77efb019d76655301a485
robot <robot@superVideo.com>
2023-01-02 23:36:53 +0800

    aws S3

--- src/main/java/net/chenlin/dp/common/entity/AwsConfig.java
+++ src/main/java/net/chenlin/dp/common/entity/AwsConfig.java
@@ -5,20 +5,28 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import lombok.AllArgsConstructor;
+import net.chenlin.dp.modules.sys.service.impl.FileSystemServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Resource;
+
 @Configuration
-@AllArgsConstructor
 public class AwsConfig {
 
+    @Resource
     private S3Model s3Model;
+
+    @Value("${file-system-active}")
+    private String fileSystemActive;
 
     @Bean
     public AmazonS3 amazonS3() {
+        if (FileSystemServiceImpl.OSS.equals(fileSystemActive)){
+            return null;
+        }
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(s3Model.getAccessKeyId(), s3Model.getAccessKeySecret());
-        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_1).withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
-        return amazonS3;
+        return AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_1).withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
     }
 }
